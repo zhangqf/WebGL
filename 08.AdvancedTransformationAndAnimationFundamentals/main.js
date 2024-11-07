f8c2cecfa476cda98a0a6f1e7f6cac004488175b
zhangqf <15893652937@163.com>
2024-11-07 18:10:01 +0800

    高级变换与动画基础 Matrix4对象

--- 08.AdvancedTransformationAndAnimationFundamentals/main.js
+++ 08.AdvancedTransformationAndAnimationFundamentals/main.js
@@ -1,28 +1,28 @@
 const VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
-    'uniform vec4 u_Translation;\n' +
+    'uniform mat4 u_xformMatrix;\n' +
     'void main() {\n' +
-        'gl_Position = a_Position + u_Translation;\n' +
+    'gl_Position = u_xformMatrix * a_Position;\n' +
+    // 'gl_PointSize = 10.0;\n' +
     '}\n'
 const FSHADER_SOURCE =
-   ' void main() {\n'+
-        'gl_FragColor = vec4(1.0, 1.0, 0.0,1.0);\n'+
+    ' void main() {\n' +
+    'gl_FragColor = vec4(1.0, 1.0, 0.0,1.0);\n' +
     '}\n'
 
-// 在 x y z 方向上平移的距离
 
-var Tx = 0.5, Ty = 0.5, Tz= 0.0;
+var ANGLE = 150.0
 
 function main() {
     var canvas = document.getElementById('webgl')
     var gl = getWebGLContext(canvas)
-    if(!gl) {
+    if (!gl) {
         console.error('Failed to get the rendering context for WebGL')
         return;
     }
 
     // 初始化着色器
-    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.error('Failed to initialize shaders.')
         return;
     }
@@ -30,20 +30,25 @@ function main() {
     // 设置顶点着色器
     var n = initVertexBuffers(gl);
 
-
-    var u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
-    gl.uniform4f(u_Translation, Tx, Ty, Tz, 0.0)
-
-    if(n < 0) {
+    if (n < 0) {
         console.error('Failed to set the positions of the vertices')
         return;
     }
+
+    var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix')
+
+    var xformMatrix = new Matrix4()
+    xformMatrix.setRotate(ANGLE, 0, 0, 1)
+
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements)
+
+
 
     // // 获取attribut变量的存储位置
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position')
     //
     // var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
-    if(a_Position < 0) {
+    if (a_Position < 0) {
         console.error('Failed to get the storage location of a_Position')
         return;
     }
@@ -51,18 +56,16 @@ function main() {
     // canvas.onmousedown = function (ev) {click(ev, gl, canvas, a_Position, u_FragColor)}
 
 
-
     // gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0)
 
     // 设置canvas背景色
-    gl.clearColor(0.0,0.0,0.0,1.0)
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
 
     // 清空canvas
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     // 绘制三个点
     gl.drawArrays(gl.TRIANGLE_FAN, 0, n)
-
 
 }
 
@@ -74,7 +77,7 @@ function initVertexBuffers(gl) {
 
     // 创建缓冲区对象
     var vertexBuffer = gl.createBuffer();
-    if(!vertexBuffer) {
+    if (!vertexBuffer) {
         console.error('Failed to create the buffer object')
         return -1;
     }
