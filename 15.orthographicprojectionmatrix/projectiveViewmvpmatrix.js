cbb4aa6cb446b5b28c8c9849d5ce7ffd6a157747
zhangfeng <15893652937@163.com>
2024-12-11 18:15:44 +0800

    三维

--- 15.orthographicprojectionmatrix/projectiveViewmvpmatrix.js
+++ 15.orthographicprojectionmatrix/projectiveViewmvpmatrix.js
@@ -1,70 +1,58 @@
-const VSHADER_SOURCE=
+const VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute vec4 a_Color;\n' +
-    'uniform mat4 u_ViewMatrix;\n' +
-    'uniform mat4 u_ProjMatrix;\n' +
-    'uniform mat4 u_ModelMatrix;\n' +
+    'uniform mat4 u_MvpMatrix;\n' +
     'varying vec4 v_Color;\n' +
     'void main(){\n' +
-    'gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;\n' +
+    'gl_Position = u_MvpMatrix * a_Position;\n' +
     'v_Color = a_Color;\n' +
-    '}\n';
+    '}\n'
 const FSHADER_SOURCE =
     '#ifdef GL_ES\n' +
-    'precision mediump float;\n' +
+    'percision mediump float;\n' +
     '#endif\n' +
     'varying vec4 v_Color;\n' +
     'void main(){\n' +
     'gl_FragColor = v_Color;\n' +
-    '}\n';
+    '}\n'
 
-function main() {
+function main(){
     const canvas = document.querySelector('canvas')
     const gl = canvas.getContext('webgl')
     if(!gl) {
         console.error('Unable to initialize WebGL.')
         return
     }
-
     if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.error('Failed to initialize shaders.')
         return;
     }
-
     const n = initVertexBuffers(gl)
-    if(n < 0) {
-        console.error('Failed to set the positions of the vertices.')
-        return;
-    }
 
-    const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix')
-    const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix')
-    const u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix')
 
-    const modelMatrix = new Matrix4()
+    const mvpMatrix = new Matrix4()
     const viewMatrix = new Matrix4()
     const projMatrix = new Matrix4()
+    const modelMatrix = new Matrix4()
 
-    modelMatrix.setTranslate(0.75,0,0)
-
-    projMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100)
-
+    modelMatrix.setTranslate(0.75, 0, 0)
     viewMatrix.setLookAt(0,0,5,0,0,-100,0,1,0)
+    projMatrix.setPerspective(30, canvas.width / canvas.height, 1, 100)
 
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
-    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements)
-    gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements)
+    // 计算模型视图投影矩阵
+    mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix)
 
-    gl.clearColor(0.0,0.0, 0.0, 1.0)
+    gl.uniformMatrix4fv(u_MvpMatrix, false, modelMatrix.elements)
+
+    gl.clearColor(0,0,0,1)
     gl.clear(gl.COLOR_BUFFER_BIT)
+
     gl.drawArrays(gl.TRIANGLES, 0, n)
 
-    modelMatrix.setTranslate(-0.75, 0, 0)
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
-    gl.drawArrays(gl.TRIANGLES, 0, n)
 }
 
-function initVertexBuffers(gl) {
+function initVertexBUffers(gl) {
     const verticesColors = new Float32Array([
         0.0, 1.0, -4.0, 0.4, 1.0, 0.4,
         -0.5, -1.0, -4.0, 0.4, 1.0, 0.4,
@@ -78,17 +66,19 @@ function initVertexBuffers(gl) {
         -0.5, -1.0, 0.0, 0.4, 0.4, 1.0,
         0.5, -1.0, 0.0, 1.0, 0.4, 0.4
     ])
+
     const n = 9
+
     const size = verticesColors.BYTES_PER_ELEMENT
 
     const vertexColorBuffer = gl.createBuffer()
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
 
-    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, vertexColorBuffer, gl.STATIC_DRAW)
 
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, size * 6 ,0)
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, size * 6, 0)
     gl.enableVertexAttribArray(a_Position)
 
     const a_Color = gl.getAttribLocation(gl.program, 'a_Color')
@@ -96,4 +86,5 @@ function initVertexBuffers(gl) {
     gl.enableVertexAttribArray(a_Color)
 
     return n
+
 }
