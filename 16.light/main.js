874746889d76b4989fb45ebc53f267e92764fb03
zhangfeng <15893652937@163.com>
2024-12-27 17:59:43 +0800

    光照 01

--- 16.light/main.js
+++ 16.light/main.js
@@ -1,119 +1,162 @@
-const VSHADER_SOURCE =
+const VSHADER_SOURCE=
     'attribute vec4 a_Position;\n' +
     'attribute vec4 a_Color;\n' +
-    'uniform mat4 u_ProjMatrix;\n' +
+    'attribute vec4 a_Normal;\n' +
+    'uniform mat4 u_MvpMatrix;\n' +
+    'uniform vec3 u_LightColor;\n' +
+    'uniform vec3 u_LightDirection;\n' +
     'varying vec4 v_Color;\n' +
-    'void main(){\n' +
-    'gl_Position = u_ProjMatrix * a_Position;\n' +
-    'v_Color = a_Color;\n' +
-    '}\n'
+    'void main(){' +
+    'gl_Position = u_MvpMatrix * a_Position;\n' +
+    'vec3 normal = normalize(vec3(a_Normal));\n' +
+    'float nDotL = max(dot(u_LightDirection, normal), 0.0);\n' +
+    'vec3 diffuse = u_LightColor * vec3(a_Color) * nDotL;\n' +
+    'v_Color = vec4(diffuse, a_Color.a);\n' +
+    '}'
 
 const FSHADER_SOURCE =
     '#ifdef GL_ES\n' +
     'precision mediump float;\n' +
     '#endif\n' +
     'varying vec4 v_Color;\n' +
-    'void main(){\n' +
+    'void main(){' +
     'gl_FragColor = v_Color;\n' +
-    '}\n'
+    '}'
 
 function main() {
     const canvas = document.querySelector('canvas')
-    const nf = document.getElementById('nearFar')
-
     const gl = canvas.getContext('webgl')
-
     if(!gl) {
         console.error('Unable to initialize WebGL.')
         return
     }
 
-    if(!initShaders(gl, VSHADER_SOURCE,FSHADER_SOURCE)) {
+    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.error('Failed to initialize shaders.')
+        return;
     }
-
-    const n = initVertexBuffers(gl);
+    const n = initVertexBuffers(gl)
 
     if(n < 0) {
-        console.error('Faild to initialize shaders.')
-    }
-    const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix')
+        console.error('Failed to initvertexBuffer ')
 
-    const projMatrix = new Matrix4()
-    document.onkeydown = function (ev) {
-        onkeydown(ev, gl, n, u_ProjMatrix, projMatrix, nf)
+        return;
     }
 
-    draw(gl,n,u_ProjMatrix, projMatrix,nf)
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.enable(gl.DEPTH_TEST)
 
+    const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix')
 
-}
+    const u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor')
 
-let g_near = 0.0, g_far=0.5;
-function onkeydown(ev, gl, n, u_ProjMatrix, projMatrix,nf) {
-    switch (ev.keyCode) {
-        case 39: g_near += 0.01; break;
-        case 37: g_near -= 0.01; break;
-        case 38: g_far += 0.01; break;
-        case 40: g_far -= 0.01; break;
-        default: return;
-    }
-    draw(gl, n, u_ProjMatrix, projMatrix, nf)
+    const u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection')
+
+    // 设置光线的颜色
+    gl.uniform3f(u_LightColor,1.0, 1.0, 1.0)
+
+    // 设置光线的方向
+    const lightDirection = new Vector3([0.5, 3.0, 4.0])
+
+    lightDirection.normalize();
+
+    gl.uniform3fv(u_LightDirection, lightDirection.elements)
+
+    const mvpMatrix = new Matrix4()
+
+    mvpMatrix.setPerspective(30, 1, 1, 100)
+
+    mvpMatrix.lookAt(3,3,7,0,0,0,0,1,0)
+
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements)
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    gl.drawElements(gl.TRIANGLES,n,gl.UNSIGNED_BYTE,0)
 }
 
 function initVertexBuffers(gl) {
-    const verticesColors = new Float32Array([
-        // 最后边绿色三角形
-        0.0, 0.5, -0.4, 0.4, 1.0, 0.4,
-        -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
-        0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+    const vertices = new Float32Array([
+        // front
+        1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,1.0, -1.0, 1.0, // 0 1 2 3
 
-        // 中间的黄色三角形
-        0.5, 0.4, -0.2, 1.0, 0.4, 0.4,
-        -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
-        0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+        // right
+        1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, // 0 3 4 5
 
-        // 最前边的蓝色山角形
-        0.0, 0.5, 0.0, 0.4, 0.4, 1.0,
-        -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
-        0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
+        // up
+        1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, // 0 5 6 2
+
+        // left
+        -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0,-1.0, -1.0, 1.0, // 1 6 7 2
+
+        //down
+        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0,-1.0, -1.0, 1.0, // 7 4 3 2
+
+
+        //back
+        -1.0, -1.0, -1.0,  1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0// 7 4 5 6
+
     ])
-    const n = 9
-    const size = verticesColors.BYTES_PER_ELEMENT
+    const colors = new Float32Array([
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+    ])
 
-    // 创建缓冲区
-    const vertexColorBuffer = gl.createBuffer()
-    // 绑定缓冲区
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer)
-    // 向缓冲区写入数据
-    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW)
+    const indices = new Uint8Array([
+        0, 1, 2, 0, 2, 3, // 前
+        4, 5, 6, 4, 6, 7, // 右
+        8,9,10, 8,10,11, // 上
+        12, 13, 14, 12, 14, 15, // 左
+        16, 17, 18, 16, 18, 19, // 下
+        20, 21, 22, 20, 22, 23, // 后
+    ])
 
-    // 获取attribute变量
-    // const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-    const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
-    // 将缓冲区数据分配给变量
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, size * 6, 0)
-    // 开启变量 方便着色器访问缓冲区内的数据
-    gl.enableVertexAttribArray(a_Position)
+    const normals = new Float32Array([
+        0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+    ])
 
-    const a_Color = gl.getAttribLocation(gl.program, 'a_Color')
-    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, size * 6, size*3)
-    gl.enableVertexAttribArray(a_Color)
+    if(!initArrayBuffer(gl,  normals, 3, gl.FLOAT)) {
+        return  -1;
+    }
+    if(!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position')) {
+        console.log(32)
+        return  -1;
+    }
 
-    return n
+
+    if(!initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color')) {
+        console.log(43)
+        return -1;
+    }
+
+    const indexBuffer = gl.createBuffer()
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+    return indices.length;
+
+
 }
 
-function draw(gl,n, u_ProjMatrix, projMatrix,nf) {
+function initArrayBuffer(gl, data, num, type, attribute) {
+    const buffer = gl.createBuffer()
 
-    projMatrix.setOrtho(-1,1,-1,1, g_near ,g_far)
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
 
-    gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements)
+    const a_attribute = gl.getAttribLocation(gl.program, attribute)
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0)
-
-    gl.clear(gl.COLOR_BUFFER_BIT)
-
-    nf.innerHTML = 'near:' + Math.round(g_near * 100) /100 + ',far:' + Math.round(g_far * 100) /100;
-
-    gl.drawArrays(gl.TRIANGLES, 0, n)
+    gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0)
+    gl.enableVertexAttribArray(a_attribute)
+    return true
 }
